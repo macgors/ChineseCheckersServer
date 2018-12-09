9dc41177d22ab9d133bac6dca4db7ba1efd9b3da
macgors <244991@student.pwr.edu>
2018-12-09 18:01:57 +0100

    Initial commit of the Chinese Checkers Server project

---
+++ src/ChineseCheckersServer/Marbles.java
@@ -0,0 +1,26 @@
+package ChineseCheckersServer;
+
+import javafx.scene.paint.Color;
+
+public class Marbles extends javafx.scene.shape.Circle {
+
+    public void setDefaultGrayColor(Color color) {
+        this.setFill(Color.GRAY);
+    }
+
+    public void setDefaultPlayerColor(Color color) {
+        this.setFill(Color.DARKGRAY);
+    }
+
+    public void setColor(Color color) {
+        this.setFill(color);
+    }
+
+       /*
+       public void getColor(Color color) {
+            this.getColor(color);
+        }
+       I dont know why get Color should be null so i put it in comments couse it makes my life hard
+       */
+}
+
